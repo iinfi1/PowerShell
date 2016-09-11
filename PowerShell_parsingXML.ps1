42cca23f8d5173e253343d342ed8ca8b4a1e1d40
arvind <iinfi1@gmail.com>
2016-09-11 13:15:18 +0530

    code update

--- PowerShell_parsingXML.ps1
+++ PowerShell_parsingXML.ps1
@@ -1,10 +1,4 @@
-﻿
-<#
-Command to retrieve System errors in the last 24 hours and type it to a HTML file
-#>
-Get-EventLog -LogName System -EntryType Error | Where-Object {($_.TimeWritten -ge (Get-Date).AddDays(-1))}| select EventID, TimeWritten, Message | ConvertTo-Html | Out-File error.htm -Append
-
-<#Parsing XML files
+﻿<#Parsing XML files
 Output of DiskSPD run#>
 
 $x = [xml] (cat .\DiskSPD_results.xml)
@@ -14,5 +8,3 @@ $x.Results.TimeSpan.CpuUtilization.Average
 $x.Results.TimeSpan.Latency.AverageTotalMilliseconds
 $x.Results.TimeSpan.Latency.AverageReadMilliseconds
 $x.Results.TimeSpan.Latency.AverageWriteMilliseconds
-
-
