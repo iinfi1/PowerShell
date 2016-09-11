afe758d89bda82d0c30b6ffc363f08fee7aa0b77
arvind <iinfi1@gmail.com>
2016-09-11 12:46:00 +0530

    code update

---
+++ sample.ps1
@@ -0,0 +1,7 @@
+﻿
+<#
+Command to retrieve System errors in the last 24 hours and type it to a HTML file
+#>
+Get-EventLog -LogName System -EntryType Error | Where-Object {($_.TimeWritten -ge (Get-Date).AddDays(-1))}| select EventID, TimeWritten, Message | ConvertTo-Html | Out-File error.htm -Append
+
+
